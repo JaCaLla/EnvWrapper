4ce2f6dae21294653bedc686da30184e88a7f419
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-11-17 12:30:08 +0100

    Add .env to .gitignore

---
+++ EnvWrapper/EnvWrapper/EnvWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvWrapperApp.swift
+//  EnvWrapper
+//
+//  Created by Javier Calatrava on 17/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EnvWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
